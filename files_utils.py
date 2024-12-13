2d72df7084f0b783f36906d5d4a103daff3ced2c
Alex Berezovsky <oxonkoalexey@gmail.com>
2024-12-13 23:26:48 +0300

    Создал функцию для записи данных в JSON файл

--- files_utils.py
+++ files_utils.py
@@ -11,3 +11,13 @@ def read_json(file_path: str, encoding: str = "utf-8") -> dict:
     """
     with open(file_path, 'r', encoding=encoding) as file:
         return json.load(file)
+def write_json(data: dict, file_path: str, encoding: str = "utf-8") -> None:
+    """
+    Записывает данные в JSON-файл.
+
+    :param data: Данные для записи.
+    :param file_path: Путь к файлу.
+    :param encoding: Кодировка файла (по умолчанию "utf-8").
+    """
+    with open(file_path, 'w', encoding=encoding) as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
