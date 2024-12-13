ecab4c453889098e3ce1519de390352491c3ffc4
Alex Berezovsky <oxonkoalexey@gmail.com>
2024-12-13 23:25:46 +0300

    Создал функцию read_json для чтения данных из JSON

--- files_utils.py
+++ files_utils.py
@@ -1,3 +1,13 @@
 import json
 import csv
 import yaml
+def read_json(file_path: str, encoding: str = "utf-8") -> dict:
+    """
+    Читает данные из JSON-файла.
+
+    :param file_path: Путь к файлу.
+    :param encoding: Кодировка файла (по умолчанию "utf-8").
+    :return: Данные, считанные из файла.
+    """
+    with open(file_path, 'r', encoding=encoding) as file:
+        return json.load(file)
