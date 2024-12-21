595699e6aac4be3806038c14acca3bed6d7f8e61
Alex Berezovsky <oxonkoalexey@gmail.com>
2024-12-21 20:24:02 +0300

    Создал функцию для фильтрации словаря по введенным id

--- HW_25.py
+++ HW_25.py
@@ -10,4 +10,9 @@ def get_user_input() -> list[int | None]:
     user_input = input("Введите цифры через пробел: ")
     user_ids = user_input.split()
     return list(map(lambda x: int(x) if x.isdigit() else None, user_ids))
-
+# Функция для фильтрации словаря по введенным id
+def filter_dict_by_ids(data: dict, ids: list[int | None]) -> dict:
+    """
+    Фильтрует словарь по переданным id.
+    """
+    return {id_: data[id_] for id_ in ids if id_ in data}
