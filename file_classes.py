009280c97d20664418c34573172e9544bcf9e932
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-01-29 22:10:14 +0300

    Создание класса AbstractFile и написание его логики

--- file_classes.py
+++ file_classes.py
@@ -2,3 +2,35 @@ from abc import ABC, abstractmethod
 import json
 import csv
 from typing import List, Dict, Any, Union
+class AbstractFile(ABC):
+    """
+    Абстрактный класс для работы с файлами.
+    Определяет интерфейс для чтения, записи и добавления данных.
+    """
+
+    @abstractmethod
+    def read(self) -> Union[Dict, List, str]:
+        """
+        Абстрактный метод для чтения данных из файла.
+
+        :return: Данные из файла (зависит от типа файла).
+        """
+        pass
+
+    @abstractmethod
+    def write(self, data: Union[Dict, List, str]) -> None:
+        """
+        Абстрактный метод для записи данных в файл.
+
+        :param data: Данные для записи.
+        """
+        pass
+
+    @abstractmethod
+    def append(self, data: Union[Dict, List, str]) -> None:
+        """
+        Абстрактный метод для добавления данных в файл.
+
+        :param data: Данные для добавления.
+        """
+        pass
